a3b800f1db3ad8321c801d1ecbd9f58f7dbb9619
Misha <mishakib91@icloud.com>
2015-11-19 10:58:06 +0200

    Post & user asscoiation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.create
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to @post
     else
